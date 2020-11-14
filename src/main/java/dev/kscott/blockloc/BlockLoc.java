00fe45a1e5a37209708a3145b659a944d460ed8f
kscottdev <kscottdev@gmail.com>
2020-11-14 04:05:04 -0500

    appease proxi

--- src/main/java/dev/kscott/blockloc/BlockLoc.java
+++ src/main/java/dev/kscott/blockloc/BlockLoc.java
@@ -3,6 +3,7 @@ package dev.kscott.blockloc;
 import cloud.commandframework.Command;
 import cloud.commandframework.CommandTree;
 import cloud.commandframework.bukkit.parsers.MaterialArgument;
+import cloud.commandframework.context.CommandContext;
 import cloud.commandframework.exceptions.ArgumentParseException;
 import cloud.commandframework.execution.AsynchronousCommandExecutionCoordinator;
 import cloud.commandframework.execution.CommandExecutionCoordinator;
@@ -63,6 +64,7 @@ public final class BlockLoc extends JavaPlugin {
         if (this.worldEdit == null) {
             this.getLogger().severe("WorldEdit was not found!");
             this.getServer().getPluginManager().disablePlugin(this);
+            return;
         }
 
         constructCommands();
@@ -87,79 +89,79 @@ public final class BlockLoc extends JavaPlugin {
         this.manager.command(builder.literal("getType")
                 .senderType(Player.class)
                 .argument(MaterialArgument.of("material"))
-                .handler(ctx -> manager.taskRecipe().begin(ctx)
-                        .asynchronous(cmdCtx -> {
-                            final @NonNull Player player = (Player) cmdCtx.getSender();
-                            final @NonNull Audience playerAudience = this.bukkitAudiences.player(player);
-                            final @NonNull Material material = ctx.get("material");
-
-                            final @NonNull LocalSession localSession = worldEdit.getSession(player);
-                            final @NonNull World selectionWorld = localSession.getSelectionWorld();
-                            final @NonNull Region region;
-
-                            // Attempt to grab region from WE, if it isn't complete let the player know and return
-                            try {
-                                region = localSession.getSelection(selectionWorld);
-                            } catch (final IncompleteRegionException exception) {
-                                playerAudience.sendMessage(Component.text("You must select a full region to use this command.").color(NamedTextColor.RED));
-                                return;
-                            }
-
-                            // Tell the player we're calculating blocks
-                            playerAudience.sendMessage(Component.text("Calculating blocks...").color(NamedTextColor.GRAY));
-
-                            final @NonNull Set<String> blockLocationStrings = new HashSet<>();
-
-                            final @NonNull BlockVector3 vecMax = region.getMaximumPoint();
-                            final @NonNull BlockVector3 vecMin = region.getMinimumPoint();
-
-                            final int xMax = vecMax.getBlockX();
-                            final int yMax = vecMax.getBlockY();
-                            final int zMax = vecMax.getBlockZ();
-
-                            final int xMin = vecMin.getBlockX();
-                            final int yMin = vecMin.getBlockY();
-                            final int zMin = vecMin.getBlockZ();
-
-                            // Loop through xMin-Max, yMin-Max, zMin-Max
-                            for (int i = xMin; i <= xMax; i++) {
-                                for (int j = yMin; j <= yMax; j++) {
-                                    for (int k = zMin; k <= zMax; k++) {
-                                        // Grab block info
-                                        BlockVector3 blockVector3 = BlockVector3.at(i, j, k);
-                                        BlockState blockState = selectionWorld.getBlock(blockVector3);
-
-                                        String id = blockState.getBlockType().getId();
-
-                                        // If the blockType id matches our material key, add it to the list
-                                        if (id.equals(material.getKey().toString())) {
-                                            blockLocationStrings.add(blockVector3.getBlockX() + ", " + blockVector3.getBlockY() + ", " + blockVector3.getBlockZ());
-                                        }
-                                    }
-                                }
-                            }
-
-                            // If list is empty, tell player no blocks were found and return
-                            if (blockLocationStrings.isEmpty()) {
-                                playerAudience.sendMessage(Component.text("There were no blocks of type ").color(NamedTextColor.GRAY)
-                                        .append(Component.text(material.name()).decorate(TextDecoration.BOLD).color(NamedTextColor.YELLOW))
-                                        .append(Component.text(" in your selection.").color(NamedTextColor.GRAY).decoration(TextDecoration.BOLD, false))
-                                );
-                                return;
-                            }
-
-                            // Notify player of console print
-                            playerAudience.sendMessage(Component.text("The block location list has been printed to the console.").color(NamedTextColor.GRAY));
-
-                            // Print to console
-                            String headerText = "---------- Locations of " + material.name() + " in " + selectionWorld.getName() + " ("+blockLocationStrings.size()+")"+" ----------";
-                            this.getLogger().info(headerText);
-                            blockLocationStrings.forEach(location -> {
-                                this.getLogger().info(location);
-                            });
-                            this.getLogger().info("-".repeat(headerText.length()));
-                        }).execute()
-                )
+                .handler(ctx -> manager.taskRecipe().begin(ctx).asynchronous(this::handleGetType).execute())
         );
+    }
+
+    private void handleGetType(final @NonNull CommandContext<CommandSender> ctx) {
+        final @NonNull Player player = (Player) ctx.getSender();
+        final @NonNull Audience playerAudience = this.bukkitAudiences.player(player);
+        final @NonNull Material material = ctx.get("material");
+
+        final @NonNull LocalSession localSession = worldEdit.getSession(player);
+        final @NonNull World selectionWorld = localSession.getSelectionWorld();
+        final @NonNull Region region;
+
+        // Attempt to grab region from WE, if it isn't complete let the player know and return
+        try {
+            region = localSession.getSelection(selectionWorld);
+        } catch (final IncompleteRegionException exception) {
+            playerAudience.sendMessage(Component.text("You must select a full region to use this command.").color(NamedTextColor.RED));
+            return;
+        }
+
+        // Tell the player we're calculating blocks
+        playerAudience.sendMessage(Component.text("Calculating blocks...").color(NamedTextColor.GRAY));
+
+        final @NonNull Set<String> blockLocationStrings = new HashSet<>();
+
+        final @NonNull BlockVector3 vecMax = region.getMaximumPoint();
+        final @NonNull BlockVector3 vecMin = region.getMinimumPoint();
+
+        final int xMax = vecMax.getBlockX();
+        final int yMax = vecMax.getBlockY();
+        final int zMax = vecMax.getBlockZ();
+
+        final int xMin = vecMin.getBlockX();
+        final int yMin = vecMin.getBlockY();
+        final int zMin = vecMin.getBlockZ();
+
+        // Loop through xMin-Max, yMin-Max, zMin-Max
+        for (int i = xMin; i <= xMax; i++) {
+            for (int j = yMin; j <= yMax; j++) {
+                for (int k = zMin; k <= zMax; k++) {
+                    // Grab block info
+                    BlockVector3 blockVector3 = BlockVector3.at(i, j, k);
+                    BlockState blockState = selectionWorld.getBlock(blockVector3);
+
+                    String id = blockState.getBlockType().getId();
+
+                    // If the blockType id matches our material key, add it to the list
+                    if (id.equals(material.getKey().toString())) {
+                        blockLocationStrings.add(blockVector3.getBlockX() + ", " + blockVector3.getBlockY() + ", " + blockVector3.getBlockZ());
+                    }
+                }
+            }
+        }
+
+        // If list is empty, tell player no blocks were found and return
+        if (blockLocationStrings.isEmpty()) {
+            playerAudience.sendMessage(Component.text("There were no blocks of type ").color(NamedTextColor.GRAY)
+                    .append(Component.text(material.name()).decorate(TextDecoration.BOLD).color(NamedTextColor.YELLOW))
+                    .append(Component.text(" in your selection.").color(NamedTextColor.GRAY).decoration(TextDecoration.BOLD, false))
+            );
+            return;
+        }
+
+        // Notify player of console print
+        playerAudience.sendMessage(Component.text("The block location list has been printed to the console.").color(NamedTextColor.GRAY));
+
+        // Print to console
+        String headerText = "---------- Locations of " + material.name() + " in " + selectionWorld.getName() + " (" + blockLocationStrings.size() + ")" + " ----------";
+        this.getLogger().info(headerText);
+        blockLocationStrings.forEach(location -> {
+            this.getLogger().info(location);
+        });
+        this.getLogger().info("-".repeat(headerText.length()));
     }
 }
